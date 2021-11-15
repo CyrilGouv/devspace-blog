c1e4a750355348bddfbc8c3466775e5b42f01cf9
Cyril Gouv <hello@cyrilgouv.com>
2021-11-15 17:06:37 +0100

    display single blog post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,13 +1,40 @@
 import fs from 'fs'
 import path from 'path'
+import Link from 'next/link'
 import matter from 'gray-matter'
+import { marked } from 'marked'
+import Layout from '../../components/Layout'
+import CategoryLabel from '../../components/CategoryLabel'
 
-export default function PostPage({ frontmatter: { title, category, content, slug, date, conver_image, author, author_image }, content, slug }) {
+export default function PostPage({ frontmatter, content, slug }) {
 
     return (
-        <div>
+        <Layout title={ frontmatter.title }>
+            <Link href="/blog">Go Back</Link>
+            <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+                <div className="flex justify-between items-center mt-4">
+                    <h1 className="text-5xl mb-7">{ frontmatter.title }</h1>
+                    <CategoryLabel>{ frontmatter.category }</CategoryLabel>
+                </div>
+
+                <img src={ frontmatter.cover_image } alt={ frontmatter.title } className="w-full rounded" />
             
-        </div>
+                <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+                    <div className="flex items-center">
+                        <img src={ frontmatter.author_image } alt={ frontmatter.author } className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block" />
+                        <h4>{ frontmatter.author }</h4>
+                    </div>
+
+                    <div className="mr-4">
+                        { frontmatter.date }
+                    </div>
+                </div>
+
+                <div className="blog-text mt-2">
+                    <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
