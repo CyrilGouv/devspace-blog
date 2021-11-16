28b8ac84e27a0806705f8c706efd35d943bf6a9a
Cyril Gouv <hello@cyrilgouv.com>
2021-11-16 16:36:43 +0100

    Next export & limitations

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -3,20 +3,28 @@ import path from 'path'
 import matter from 'gray-matter'
 import Layout from "@/components/Layout"
 import Post from '@/components/Post'
+import CategoryList from '@/components/CategoryList'
 import { getPosts } from '@/lib/posts'
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
 
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-blod">Posts in { categoryName }</h1>
-    
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {
-          posts.map((post, index) => (
-            <Post key={ index } post={ post } />
-          ))
-        }
+      <div className="flex justify-between">
+          <div className="w-3/4 mr-10">
+            <h1 className="text-5xl border-b-4 p-5 font-blod">Posts in { categoryName }</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+              {
+                posts.map((post, index) => (
+                  <Post key={ index } post={ post } />
+                ))
+              }
+            </div>
+          </div>
+
+          <div className="w-1/4">
+              <CategoryList categories={ categories } />
+          </div>
       </div>
     </Layout>
   )
@@ -52,6 +60,10 @@ export async function getStaticProps({ params: { category_name } }) {
   
     const posts = getPosts()
 
+    // Get Categories for sidebar
+    const categories = posts.map(post => post.frontmatter.category)
+    const uniqCategories = [...new Set(categories)]
+
   // Filter post by category
   const categoryPosts = posts.filter(post => post.frontmatter.category.toLowerCase() === category_name)
 
@@ -59,6 +71,7 @@ export async function getStaticProps({ params: { category_name } }) {
         props: {
             posts: categoryPosts,
             categoryName: category_name,
+            categories: uniqCategories
 
         }
     }
